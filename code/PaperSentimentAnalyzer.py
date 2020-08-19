724cf3c39f66f65f19dbb48b1fe92372718cf70d
yang <jzy991126@outlook.com>
2020-08-19 19:13:53 +0800

    再次加快分析速度

--- code/PaperSentimentAnalyzer.py
+++ code/PaperSentimentAnalyzer.py
@@ -29,20 +29,38 @@ def get_review_data():
 def analyse(review_list):
 	datas = MyDataGenerator(review_list, tokenizer, MAX_LEN)
 	result = sentiment_predictor.predict(datas)
-	return result
+	if isinstance(result, list):
+		res = []
+		for item in result:
+			res.extend(item.tolist())
+		return res
+	return result.tolist()
 
 
 def main():
 	result = {}
 	review_data = get_review_data()
 
+	length_data = []
+	key_data = []
+	sentence_data = []
+	for key, val in tqdm(review_data.items()):
+		length_data.append(len(val))
+		key_data.append(key)
+		sentence_data.extend(val)
+
 	for aim in aim_list:
 		print(aim)
 		sentiment_predictor.load_weights(os.path.join(SENTENCE_MODEL_PATH, aim))
-		for key, val in tqdm(review_data.items()):
+		analyze_result = analyse(sentence_data)
+		now_index = 0
+		for index in range(len(key_data)):
+			key = key_data[index]
 			if key not in result:
 				result[key] = {}
-			result[key][aim] = analyse(val)
+			result[key][aim] = analyze_result[now_index:now_index + length_data[index]]
+			now_index += length_data[index]
+
 	with open('result.json', 'w') as f:
 		json.dump(result, f)
 
